1c786992b9d94e8ca59815c4841f526d79336299
Alessandro <volcich.alessandro@hotmail.com>
2018-05-31 17:34:29 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using FirstLocalDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 db = new Class1();
+            Console.WriteLine("Hello");
+            Console.WriteLine("Running new query");
+            foreach (var item in db.QueryAll())
+            {
+                Console.WriteLine(String.Format("{0}-{1}-{2}",item.CustomerName,item.CustomerContact,item.Phone));
+            }
+            Console.WriteLine("Query finished.....");
+            Console.WriteLine("Inserting new data");
+            db.Add("new Customer", "new Contact", "000-000-2323");
+            Console.WriteLine("new entry added");         
+            foreach (var item in db.QueryAll())
+            {
+                Console.WriteLine(String.Format("{0}-{1}-{2}", item.CustomerName, item.CustomerContact, item.Phone));
+            }
+            Console.WriteLine("Query finished.....");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ FirstLocalDb/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLocalDb
+{
+    public class Class1
+    {
+
+
+        //DbProvider-ToDo
+        //TODO Initialize DB-Model Object 
+        SampleDbEntities dbmodel = new SampleDbEntities();
+        //TODO Write Linq-Queries
+
+
+        //TODO LinqInsert
+        public int Add(string s, string c, string p)
+        {
+            dbmodel.Customers.Add(new Customer()
+            {
+                CustomerName = s,
+                CustomerContact = c,
+                Phone = p
+            });
+
+            return dbmodel.SaveChanges();
+        }
+
+
+        //TODO LinqQueryAll
+        public List<Customer> QueryAll()
+        {
+            //var query = (from b in dbmodel.Customers
+            //             select new Customer
+            //             {
+            //                 CustomerName = b.CustomerName,
+            //                 CustomerContact = b.CustomerContact,
+            //                 Phone = b.Phone
+            //             });
+
+            var query = (from b in dbmodel.Customers
+                         select new {
+                            Name = b.CustomerName,
+                            Contact = b.CustomerContact,
+                            Phone = b.Phone
+                         }).ToList().Select(x => new Customer {
+                                CustomerName = x.Name,
+                             CustomerContact = x.Contact,
+                             Phone = x.Phone });
+                         
+
+            return query.ToList();
+            //return query.ToString();
+        }
+    }
+}
